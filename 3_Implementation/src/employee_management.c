e6b421619f346d03b38a1c8a521bbe0645645ba1
Apple <annapurnadvs@gmail.com>
2021-04-14 00:41:20 +0530

    #3 Completed adding edit and delete functions

--- 3_Implementation/src/employee_management.c
+++ 3_Implementation/src/employee_management.c
@@ -13,6 +13,7 @@ struct employee
 } employee;
 int num;
 struct employee emp[max], tempemp[max], sortemp[max], sortemp1[max];
+
 void menu()
 {
 	// system("clear");
@@ -20,14 +21,19 @@ void menu()
 	printf("\n***  Employees Management System 1.0 *** ");
 	printf("\n");
 	printf("             ");
-	printf("\nPress  b---->Built The Employee Table ");
+	printf("\nPress  b----> Built The Employee Table ");
 	printf("             ");
-	printf("\nPress  l---->List The Employee Table  ");
+	printf("\nPress  l----> List The Employee Table  ");
 	printf("             ");
-	printf("\nPress  q---------->Quit Program              ");
+	printf("\nPress  i----> Insert New Entry        ");
+	printf("             ");
+	printf("\nPress  d----> Delete An Entry         ");
+	printf("             ");
+	printf("\nPress  q----------> Quit Program              ");
 	printf("             ");
 	printf("\n\nSelect Your Option Please ====> ");
 }
+
 void build()
 {
 	system("clear");
@@ -54,12 +60,11 @@ void build()
 	}
 	printf("Going to main menu");
 }
+
 void list()
 {
-	//system("clear");
 	int i, j;
 	char ch;
-	//char k='k';
 	printf("         ***List The Table***");
 	printf("\n");
 	printf("Name            Code        Years(EXP)      Age   Designation\n");
@@ -80,6 +85,74 @@ void list()
 	}
 }
 
+void insert()
+{
+	system("clear");
+	int i = num;
+	num += 1;
+	printf("Insert New Record\n");
+	printf("Enter The Following Items\n");
+	printf("Name:  ");
+	scanf("%s", emp[i].name);
+	printf("empID: ");
+	scanf("%ld", &emp[i].empID);
+	printf("Designation: ");
+	scanf("%s", emp[i].designation);
+	printf("Years of Experience: ");
+	scanf("%d", &emp[i].experience);
+	printf("Age: ");
+	scanf("%d", &emp[i].age);
+	printf("\n");
+	printf("The Record has been inserted successfully\n");
+	printf("Going to main menu");
+}
+
+void delete ()
+{
+	system("clear");
+	int code;
+	int check=-1;
+	printf("Delete An Entry\n");
+	printf("Enter An JobCode(empID) To Delete That Entry:  \n");
+	scanf("%d", &code);
+	int i;
+	for (i = 0; i <= num - 1; i++)
+	{
+		if (emp[i].empID == code)
+		{
+			check = i;
+		}
+	}
+	if(check==-1)
+	{
+		printf("Unable to find the Record.Please try again...\n");
+	}
+	for (i = 0; i <= num - 1; i++)
+	{
+		if (i == check)
+		{
+			printf("The Record with this EmpID is deleted.");
+			continue;
+		}
+		else
+		{
+			if (i > check)
+			{
+				tempemp[i - 1] = emp[i];
+			}
+			else
+			{
+				tempemp[i] = emp[i];
+			}
+		}
+	}
+	num--;
+	for (i = 0; i <= num - 1; i++)
+	{
+		emp[i] = tempemp[i];
+	}
+}
+
 int main()
 {
 	system("clear");
@@ -97,6 +170,14 @@ int main()
 			break;
 		case 'l':
 			list();
+			menu();
+			break;
+		case 'i':
+			insert();
+			menu();
+			break;
+		case 'd':
+			delete ();
 			menu();
 			break;
 		}
